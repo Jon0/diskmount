b81dafa7a5b4fc3c0b6a6ba287185fea17548a42
Jon0 <Jono4728@gmail.com>
2015-12-18 22:10:14 +1300

    mkdir

--- diskadd.py
+++ diskadd.py
@@ -17,14 +17,17 @@ def find_env(keys):
 
 label = find_env(['ID_FS_LABEL', 'ID_FS_UUID'])
 devpath = find_env(['DEVNAME', 'DEVPATH'])
+mkdir_cmd = str("mkdir /media/" + label).strip()
 mnt_cmd = str("mount " + devpath + " /media/" + label).strip()
 
 # write debug file
 tempfile = open('/tmp/devices', 'w')
 tempfile.write("device %s @ %s\n" % (label, devpath))
+tempfile.write("exec %s\n" % (mkdir_cmd))
 tempfile.write("exec %s\n" % (mnt_cmd))
 tempfile.write(exec_cmd('env'))
 tempfile.close()
 
 if (len(label) > 0 and len(devpath) > 0):
+	exec_cmd(mkdir_cmd)
 	exec_cmd(mnt_cmd)
